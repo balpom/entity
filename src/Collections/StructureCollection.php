49497de73876d8b9c077c3823817a0c2b591f18c
Mikhail <141855513+balpom@users.noreply.github.com>
2025-07-30 14:14:37 +0300

    Added new feature
    Added StructureCollectionInterface and it simple realisation.

---
+++ src/Collections/StructureCollection.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Balpom\Entity\Collections;
+
+use Balpom\Entity\StructureInterface;
+use Balpom\Entity\StructureCollectionInterface;
+
+class StructureCollection implements StructureCollectionInterface
+{
+    const TYPE = StructureInterface::class; // May be changed in children classes.
+
+    protected array $collection = [];
+    private int $position = 0;
+
+    public function add(StructureInterface $structure): void
+    {
+        $this->collection[] = $structure;
+    }
+
+    public function current(): StructureInterface
+    {
+        return $this->collection[$this->position];
+    }
+
+    public function key(): mixed
+    {
+        return $this->position;
+    }
+
+    public function next(): void
+    {
+        ++$this->position;
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->collection[$this->position]);
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->collection[$offset]);
+    }
+
+    public function offsetGet(mixed $offset): StructureInterface|null
+    {
+        return isset($this->collection[$offset]) ? $this->collection[$offset] : null;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $class = StructureCollection::TYPE;
+        if (!is_object($value) || !($value instanceof $class)) {
+            throw new StructureCollectionException('Given value must be instance of ' . $class);
+        }
+
+        if (is_null($offset)) {
+            $this->collection[] = $value;
+        } else {
+            $this->collection[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        if (isset($this->collection[$offset])) {
+            unset($this->collection[$offset]);
+        }
+    }
+
+}
---
+++ src/StructureCollectionInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Balpom\Entity;
+
+use Balpom\Entity\StructureInterface;
+
+interface StructureCollectionInterface extends \Iterator, \ArrayAccess
+{
+    public function add(StructureInterface $structure): void;
+    public function current(): StructureInterface;
+    public function key(): mixed;
+    public function next(): void;
+    public function rewind(): void;
+    public function valid(): bool;
+    public function offsetExists(mixed $offset): bool;
+    public function offsetGet(mixed $offset): StructureInterface|null;
+    public function offsetSet(mixed $offset, mixed $value): void;
+    public function offsetUnset(mixed $offset): void;
+}

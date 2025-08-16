12d80b5b5787ce7baaf0c2df89229ebe46ef5fac
Mikhail <141855513+balpom@users.noreply.github.com>
2025-08-16 07:55:44 +0300

    Interface extended.

--- src/StructureInterface.php
+++ src/StructureInterface.php
@@ -28,4 +28,22 @@ interface StructureInterface
      * Returns TRUE if $field exists, returns FALSE if $field not exist.
      */
     public function hasField(string|int|float $field): bool;
+
+    /*
+     * getField('title') returns:
+      array ['string' => true]
+     */
+    public function getField(string|int|float $field): array;
+
+    /*
+     * getFieldType('title') returns: 'string'
+     * Returning value may be only 'string', 'array', 'object', 'bool', 'int', 'float'
+     */
+    public function getFieldType(string|int|float $field): string;
+
+    /*
+     * isFieldMandatory('title') returns: true
+     * isFieldMandatory('description') returns: false
+     */
+    public function isFieldMandatory(string|int|float $field): bool;
 }
